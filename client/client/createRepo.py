5ea648b3549c9192e47a24a190bf847e01327cb0
Anupam Sanghi <kool.miths1108@gmial.com>
2012-12-15 01:04:43 +0530

    reverse proxy and filesystem server added

--- client/client/createRepo.py
+++
@@ -1,8 +0,0 @@
-import os
-import errno
-if __name__=="__main__":
-	try:
-		os.mkdir('Sync-n-Share')
-	except OSError as exception:
-		if exception.errno != errno.EEXIST:
-			raise
--- client/server/authenticate.py
+++
@@ -1,6 +0,0 @@
-def auth(data, db):
-	cur = db.cursor() 
-	userinfo = data.split(',')
-	cur.execute("SELECT * FROM user_info WHERE username = '"+userinfo[0]+"' and password = '"+userinfo[1]+"'")
-	resultset = cur.fetchall()
-	return str(len(resultset))
